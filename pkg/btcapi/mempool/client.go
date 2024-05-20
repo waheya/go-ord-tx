ecf55258cf37b1677ece7581a63075b3da7ff188
omen2 <omen@nextxlabs.com>
2024-05-20 22:21:24 +0800

    add custom base uri in mempool

--- pkg/btcapi/mempool/client.go
+++ pkg/btcapi/mempool/client.go
@@ -1,28 +1,32 @@
 package mempool
 
 import (
+	"io"
+	"log"
+
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/vincentdebug/go-ord-tx/pkg/btcapi"
-	"io"
-	"log"
 )
 
 type MempoolClient struct {
 	baseURL string
 }
 
-func NewClient(netParams *chaincfg.Params) *MempoolClient {
-	baseURL := ""
-	if netParams.Net == wire.MainNet {
-		baseURL = "https://mempool.space/api"
-	} else if netParams.Net == wire.TestNet3 {
-		baseURL = "https://mempool.space/testnet/api"
-	} else if netParams.Net == chaincfg.SigNetParams.Net {
-		baseURL = "https://mempool.space/signet/api"
-	} else {
-		log.Fatal("mempool don't support other netParams")
+func NewClient(netParams *chaincfg.Params, customBaseURL string) *MempoolClient {
+	baseURL := customBaseURL
+	if baseURL == "" {
+		if netParams.Net == wire.MainNet {
+			baseURL = "https://mempool.space/api"
+		} else if netParams.Net == wire.TestNet3 {
+			baseURL = "https://mempool.space/testnet/api"
+		} else if netParams.Net == chaincfg.SigNetParams.Net {
+			baseURL = "https://mempool.space/signet/api"
+		} else {
+			log.Fatal("mempool don't support other netParams")
+		}
 	}
+
 	return &MempoolClient{
 		baseURL: baseURL,
 	}
